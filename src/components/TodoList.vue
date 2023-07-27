9f0e0ccf9465d7eec8975e030b4c7f6419778c41
Collapsare <alexandr0899@mail.ru>
2023-07-27 15:32:01 +0300

    Created TodoList appearance realisation

---
+++ src/components/TodoList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <TodoListItem
+      v-for="item in todos"
+      :key="item.id"
+      :item="item"
+      class="todoItem"
+      @changeFinished="changeTodoFinished"
+      @itemDeleted="deleteItem"
+    />
+  </div>
+</template>
+
+<script>
+import TodoListItem from "./TodoListItem.vue";
+
+export default {
+  name: "TodoList",
+  components: {
+    TodoListItem,
+  },
+  props: {
+    todos: {
+      required: true,
+      default: () => [],
+      type: Array,
+    },
+  },
+  methods: {
+    changeTodoFinished(id) {
+      this.$emit("changeFinished", id);
+    },
+    deleteItem(id) {
+      this.$emit("itemDeleted", id);
+    },
+  },
+};
+</script>
+
+<style></style>
