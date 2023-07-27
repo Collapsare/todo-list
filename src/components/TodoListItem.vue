5803630a18c7ebc15642351fe6c425ea658fea9f
Collapsare <alexandr0899@mail.ru>
2023-07-27 15:31:04 +0300

    Created TodoListItem

---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <label class="todoItemText">
+      <input
+        type="checkbox"
+        :value="item.value"
+        @input="changeTodoFinished(item.id)"
+      />
+      <span>{{ item.name }}</span>
+    </label>
+    <button class="deleteBtn" @click="deleteItem(item.id)"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoListItem",
+  props: {
+    item: {
+      required: true,
+      default: () => {},
+      type: Object,
+    },
+  },
+  methods: {
+    changeTodoFinished(id) {
+      this.$emit("changeFinished", id);
+    },
+    deleteItem(id) {
+      this.$emit("itemDeleted", id);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
